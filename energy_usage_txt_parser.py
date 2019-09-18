f5dc11fcc41ddb08888d774ecd2562858b140774
Peter Tempfli <tempflip@gmail.com>
2019-09-18 10:34:40 +0200

    w

--- energy_usage_txt_parser.py
+++ energy_usage_txt_parser.py
@@ -9,8 +9,62 @@ MONTH_LINE = '((?:\d+ )?\d+)2018 ([a-z]{3})'
 MONTH_LINE_2 = '(\d+)2018 ([a-z]{3})(\d+)2018 ([a-z]{3}.*)'
 TOTAL_LINE = '((?:\d+ )?\d+)Totalt'
 
+METRIC_M3 = 'm3Period'
+METRIC_MWH = 'MWh'
+METRIC_KWH = 'kWhPeriod'
 
-page_list = []
+class ParsedPage:
+    page_num = None
+    address = None
+    total = None
+    month_map = {}
+    metric = None
+
+    def __init__(self, page_lines):
+        for line in page_lines:
+            self.match_a_line(line)
+    
+    def match_a_line(self, line):
+        if re.match(TITLE, line): 
+            pass
+        elif re.match(PAGE_NUM, line) : 
+            self.page_num = re.match(PAGE_NUM, line).group(1)
+            pass
+        elif re.match(ADDRESS, line) : 
+            self.address = line
+            pass
+        elif re.match(MONTH_LINE, line) : 
+            match = re.match(MONTH_LINE, line)
+            month = match.group(2)
+            num = match.group(1)
+            self.month_map[month] = num
+            # print ("MONTH_LINE:\t\t", month, '\t', num)
+            pass
+        elif re.match(TOTAL_LINE, line) : 
+            match = re.match(TOTAL_LINE, line)
+            self.total = match.group(1)
+            # print("TOTAL_LINE:\t\t\t", num)
+            pass
+        elif re.match(METRIC_M3, line) : 
+            self.metric = 'm3'
+            # print ("## METRIC\t\t m3")
+        elif re.match(METRIC_KWH, line) : 
+            self.metric = 'kwh'
+            # print ("## METRIC\t\t kwh")
+        elif re.match(METRIC_MWH, line) : 
+            self.metric = 'mwh'
+            # print ("## METRIC\t\t mwh")
+        else :
+            print ('#NO MATCH', line)
+            pass
+
+    
+    def print(self):
+        print('PAGE NUM\t\t', self.page_num)
+        print('ADDRESS\t\t', self.address)
+        print('TOTAL\t\t', self.total)
+        print('METRIC\t\t', self.metric)
+        print('month_map\t\t', self.month_map)
 
 def pre_process(page):
     pp = page
@@ -33,34 +87,9 @@ def pre_process(page):
         pp = p
     return p
 
-def parse_page(page):
 
-
-    for line in page:
-        if re.match(TITLE, line): 
-            pass
-        elif re.match(PAGE_NUM, line) : 
-            print ("PAGE NUM:\t\t", re.match(PAGE_NUM, line).group(1))
-            pass
-        elif re.match(ADDRESS, line) : 
-            print ("ADDRESS:\t\t", line)
-            pass
-        elif re.match(MONTH_LINE, line) : 
-            match = re.match(MONTH_LINE, line)
-            month = match.group(2)
-            num = match.group(1)
-            
-            print ("MONTH_LINE:\t\t", month, '\t', num)
-            pass
-        elif re.match(TOTAL_LINE, line) : 
-            match = re.match(TOTAL_LINE, line)
-            num = match.group(1)
-            print("TOTAL_LINE:\t\t\t", num)
-            pass
-        else :
-            print ('#NO MATCH', line)
-            pass
 ### organizing to pages
+page_list = []
 with open("energy_usage.txt", "r") as f:
     ll = []
     for line in f:
@@ -71,7 +100,6 @@ with open("energy_usage.txt", "r") as f:
             ll.append(line)
 
 ### parse individual pages
-# for page in page_list:
-page = page_list[4]
-p = pre_process(page)
-parse_page(p)
+for page in page_list:
+    p = ParsedPage(page)
+    p.print()
