770bbe921ec2dbdc213ef50410d6199389c54733
Peter Tempfli <tempflip@gmail.com>
2019-09-23 17:08:34 +0200

    w

--- csv_parser.py
+++ csv_parser.py
@@ -14,10 +14,11 @@ with open(ADDRESSES) as f:
 # print (addr_by_key)
 # exit()
 
-ANL = "(\d+ \(anl\))"
+ANL = "(\d+ \(anl\)).+\((m3|MWh|kWh)\)"
 DATE_LINE = '\d\d\d\d \D\D\D'
+NUMEX = '((\d+)(,| ))?(\d+)'
 
-i = 0
+counter = 0
 key = None
 page_num = None
 address = None
@@ -26,25 +27,36 @@ with open(PATH) as f:
     for row in reader:
         if re.match(ANL, row[0]) :
             key = re.match(ANL, row[0]).group(1)
+            metric = re.match(ANL, row[0]).group(2)
             page_num = addr_by_key[key][0]
             address = addr_by_key[key][1]
         
         if re.match(DATE_LINE, row[0]):
-            # print(row)
-            # if re.match('\n', row[0], re.MULTILINE):
-            # print(row[0])
             if (len(row[0]) > 10) :
                 date_line = row[0].split('\n')
                 num_line = row[3].split('\n')
                 try :
                     for j, dt in enumerate(date_line):
-                        print(page_num + ';;' + 'EXTRA_PARSING' + ';' + address + ';' + dt + ';' + num_line[j])
+                        print(page_num + ';' + metric +';;' + 'EXTRA_PARSING' + ';' + address + ';' + dt + ';' + num_line[j])
                 except IndexError:
                     pass
                 pass
             else :
-                print(page_num + ';' + ';' + key + ';' + address + ';', end = '')
-                print(';'.join(row))
+                dt = row[0].lower()
+                num = 'NO_NUM'
+                for i in range(1, len(row)):
+                    match = re.match(NUMEX, row[i])
+                    if match:
+                        if match.group(2):
+                            num = match.group(2) + match.group(4)
+                        else:
+                            num = match.group(4)
+                        # num = row[i] + ' || ' + num
+                        break
+                
+                print(page_num + ';' + metric + ';' + ';' + key + ';' + address + ';' + dt + ';' + num, end = '\n')
+                
+                # print(';'.join(row))
                 pass
-        i+= 1
-        # if (i > 200): exit()
+        counter+= 1
+        # if (counter > 1000): exit()
